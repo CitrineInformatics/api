68db8a99e3c872693e281aa96cdcaf5857c3f2d1
kjaym <kylemichel@gmail.com>
2018-01-23 23:24:14 -0800

    update swagger ui to version that supports openapi 3.0

--- scripts/build.js
+++ scripts/build.js
@@ -11,4 +11,5 @@ shell.cp('-R', 'spec/*', 'web_deploy/');
 var SWAGGER_UI_DIST = path.dirname(require.resolve('swagger-ui'));
 shell.rm('-rf', 'web_deploy/swagger-ui/')
 shell.cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
+shell.cp('web/swagger-ui/index.html', 'web_deploy/swagger-ui/')
 shell.sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../openapi.yaml', 'web_deploy/swagger-ui/index.html')
