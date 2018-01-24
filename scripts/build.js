03b99f29861c71b527349406e2560d1c5044b50b
kjaym <kylemichel@gmail.com>
2018-01-24 00:18:46 -0800

    user swagger-ui-dist package

--- scripts/build.js
+++ scripts/build.js
@@ -8,8 +8,7 @@ shell.mkdir('-p', 'web_deploy')
 shell.cp('-R', 'web/*', 'web_deploy/');
 shell.cp('-R', 'spec/*', 'web_deploy/');
 
-var SWAGGER_UI_DIST = path.dirname(require.resolve('swagger-ui'));
+var SWAGGER_UI_DIST = path.dirname(require.resolve('swagger-ui-dist'));
 shell.rm('-rf', 'web_deploy/swagger-ui/')
 shell.cp('-R', SWAGGER_UI_DIST, 'web_deploy/swagger-ui/')
-shell.cp('web/swagger-ui/index.html', 'web_deploy/swagger-ui/')
 shell.sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../openapi.yaml', 'web_deploy/swagger-ui/index.html')
