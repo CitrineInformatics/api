b37716dacc40a22534d2e78b4f118de2fbfe2e2d
kjaym <kylemichel@gmail.com>
2018-01-23 20:53:27 -0800

    generate swaggerui for branches

--- scripts/deploy-branch.js
+++ scripts/deploy-branch.js
@@ -8,9 +8,18 @@ set('-v');
 
 var branch = process.env.TRAVIS_BRANCH && process.env.TRAVIS_BRANCH.toLowerCase();
 if (branch && branch !== 'gh-pages') {
+  
+  // Setup ReDoc
   var branchPath = path.join('.tmp', 'preview', branch, '/');
   mkdir('-p', branchPath);
   cp('web/index.html', branchPath);
   cp('-R', 'spec/*', branchPath);
+  
+  // Setup SwaggerUI
+  var SWAGGER_UI_DIST = Path.dirname(require.resolve('swagger-ui'));
+  var swaggerPath = path.join(branchPath, 'swagger-ui', '/');
+  rm('-rf', swaggerPath);
+  cp('-R', SWAGGER_UI_DIST, swaggerPath);
+  sed('-i', 'http://petstore.swagger.io/v2/swagger.json', '../openapi.yaml', path.join(swaggerPath, 'index.html'));
   exec('deploy-to-gh-pages --update .tmp');
 }
